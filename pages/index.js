e02e3f2788129ead206084be6fa12ecd6818be45
Cassio Seffrin <cassioseffrin@gmail.com>
2022-06-02 21:47:23 -0300

    aula

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,24 @@
 
 function Home() {
 
-    return (<div>Pagina Vercel</div>);
+    return (<div>Pagina Vercel 2</div>);
 }
+
+
+// export async function getStaticProps() {
+//     const dataLinkTypes: LinkType[] = await linkApi.getAllLinkTypes('provin');
+//     const dataBranchs: Branch[] = await linkApi.getAllBranches('provin');
+//     let dataBranchsLV = [];
+//     let linkTYpeLV = [];
+//     dataBranchs.forEach(b => dataBranchsLV.push({ label: b.tradingName, value: b.id.toString() }));
+//     dataLinkTypes.forEach(b => linkTYpeLV.push({ label: b.description, value: b.id.toString() }));
+//     return {
+//       props: {
+//         branchs: dataBranchsLV,
+//         linkTypes: linkTYpeLV
+//       },
+//       revalidate: 3600
+//     }
+//   }
 
 export default Home
