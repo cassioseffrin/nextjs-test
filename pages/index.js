f439ddeb44addb9dab66a914bbf78e37c152d4d3
Cassio Seffrin <cassioseffrin@gmail.com>
2022-06-02 22:01:33 -0300

    repositorios

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
-
-
-
-function Home() {
-
-    return (<div>Pagina Vercel 3</div>);
+function Home(props) {
+    const repos = props.repos;
+    return (<>
+        <h2>Meus repositorios</h2>
+        {repos.array.forEach(r => {
+            <p>{r.name}</p>
+        })}
+    </>);
 }
-
-
-// export async function getStaticProps() {
-//     const dataLinkTypes: LinkType[] = await linkApi.getAllLinkTypes('provin');
-//     const dataBranchs: Branch[] = await linkApi.getAllBranches('provin');
-//     let dataBranchsLV = [];
-//     let linkTYpeLV = [];
-//     dataBranchs.forEach(b => dataBranchsLV.push({ label: b.tradingName, value: b.id.toString() }));
-//     dataLinkTypes.forEach(b => linkTYpeLV.push({ label: b.description, value: b.id.toString() }));
-//     return {
-//       props: {
-//         branchs: dataBranchsLV,
-//         linkTypes: linkTYpeLV
-//       },
-//       revalidate: 3600
-//     }
-//   }
-
+export async function getStaticProps() {
+    const URL = 'https://api.github.com/users/cassioseffrin/repos';
+    const res = await fetch(URL);
+    let data = {};
+    if (res.status == 200) {
+        data = await res.json();
+    } else {
+        data = {};
+    }
+    return {
+        props: {
+            repos: data,
+        },
+        revalidate: 3600
+    }
+}
 export default Home
